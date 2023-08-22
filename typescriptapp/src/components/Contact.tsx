d0bf17f777919865a077eeed6474ffce0f70a2b0
Echo Wang <ywang623@my.bcit.ca>
2023-08-21 17:19:29 -0700

    Refactored folder structure

--- typescriptapp/src/components/Contact.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export const Contact = () => {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Contact")
-  );
-};
--- typescriptapp/src/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-export const Home = () => {
-  return React.createElement(
-    "div",
-    null,
-    React.createElement("h1", null, "Home")
-  );
-};
